add916eb6b0d1dce0a051e2ead8d9620106f2f7e
Patrick O'Boyle <oboylepa@tcd.ie>
2016-10-23 16:21:06 +0100

    Added compiler up-to implementation of constant integers and booleans

---
+++ CocoTastierProject/Tastier.s
@@ -0,0 +1,136 @@
+; Procedure Subtract
+Subtract
+    LDR     R0, =2          ; current lexic level
+    LDR     R1, =0          ; number of local variables
+    BL      enter           ; build new stack frame
+    B       SubtractBody
+SubtractBody
+    LDR     R5, [R4]        ; i
+    LDR     R6, =1
+    SUB     R5, R5, R6
+    STR     R5, [R4]        ; i
+    MOV     TOP, BP         ; reset top of stack
+    LDR     BP, [TOP,#12]   ; and stack base pointers
+    LDR     PC, [TOP]       ; return from Subtract
+; Procedure Add
+Add
+    LDR     R0, =2          ; current lexic level
+    LDR     R1, =0          ; number of local variables
+    BL      enter           ; build new stack frame
+    B       AddBody
+AddBody
+    LDR     R5, [R4]        ; i
+    LDR     R6, =0
+    CMP     R5, R6
+    MOVGT   R5, #1
+    MOVLE   R5, #0
+    MOVS    R5, R5          ; reset Z flag in CPSR
+    BEQ     L1              ; jump on condition false
+    MOV     R0, BP          ; load current base pointer
+    LDR     R0, [R0,#8]
+    ADD     R0, R0, #16
+    LDR     R1, =1
+    LDR     R5, [R0, R1, LSL #2]        ; sum
+    LDR     R6, [R4]        ; i
+    ADD     R5, R5, R6
+    MOV     R0, BP          ; load current base pointer
+    LDR     R0, [R0,#8]
+    ADD     R0, R0, #16
+    LDR     R1, =1
+    STR     R5, [R0, R1, LSL #2]        ; sum
+    ADD     R0, PC, #4      ; store return address
+    STR     R0, [TOP]       ; in new stack frame
+    B       Subtract
+    ADD     R0, PC, #4      ; store return address
+    STR     R0, [TOP]       ; in new stack frame
+    B       Add
+    B       L2
+L1
+L2
+    MOV     TOP, BP         ; reset top of stack
+    LDR     BP, [TOP,#12]   ; and stack base pointers
+    LDR     PC, [TOP]       ; return from Add
+; Procedure SumUp
+SumUp
+    LDR     R0, =1          ; current lexic level
+    LDR     R1, =2          ; number of local variables
+    BL      enter           ; build new stack frame
+    B       SumUpBody
+SumUpBody
+    LDR     R5, [R4]        ; i
+    STR     R5, [BP,#16]    ; j
+    LDR     R5, =1
+    STR     R5, [R4]        ; k
+    MOVS    R5, #1          ; true
+    STR     R5, [R4]        ; apples
+    LDR     R5, =0
+    ADD     R0, BP, #16
+    LDR     R1, =1
+    STR     R5, [R0, R1, LSL #2]        ; sum
+    ADD     R0, PC, #4      ; store return address
+    STR     R0, [TOP]       ; in new stack frame
+    B       Add
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L3
+    DCB     "The sum of the values from 1 to ", 0
+    ALIGN
+L3
+    LDR     R5, [BP,#16]    ; j
+    MOV     R0, R5
+    BL      TastierPrintInt
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L4
+    DCB     " is ", 0
+    ALIGN
+L4
+    ADD     R0, BP, #16
+    LDR     R1, =1
+    LDR     R5, [R0, R1, LSL #2]        ; sum
+    MOV     R0, R5
+    BL      TastierPrintIntLf
+    MOV     TOP, BP         ; reset top of stack
+    LDR     BP, [TOP,#12]   ; and stack base pointers
+    LDR     PC, [TOP]       ; return from SumUp
+;Name: j, Type: integer, Kind: var, Level: local, Init: True
+;Name: sum, Type: integer, Kind: var, Level: local, Init: True
+;Name: Subtract, Type: undef, Kind: proc, Level: local, Init: True
+;Name: Add, Type: undef, Kind: proc, Level: local, Init: True
+mainline
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L5
+    DCB     "Enter value for i (or 0 to stop): ", 0
+    ALIGN
+L5
+    BL      TastierReadInt
+    STR     R0, [R4]        ; i
+L6
+    LDR     R5, [R4]        ; i
+    LDR     R6, =0
+    CMP     R5, R6
+    MOVGT   R5, #1
+    MOVLE   R5, #0
+    MOVS    R5, R5          ; reset Z flag in CPSR
+    BEQ     L7              ; jump on condition false
+    ADD     R0, PC, #4      ; store return address
+    STR     R0, [TOP]       ; in new stack frame
+    B       SumUp
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L8
+    DCB     "Enter value for i (or 0 to stop): ", 0
+    ALIGN
+L8
+    BL      TastierReadInt
+    STR     R0, [R4]        ; i
+    B       L6
+L7
+stopTest
+    B       stopTest
+;Name: k, Type: integer, Kind: constant, Level: global, Init: True
+;Name: apples, Type: boolean, Kind: constant, Level: global, Init: True
+;Name: i, Type: integer, Kind: var, Level: global, Init: True
+;Name: SumUp, Type: undef, Kind: proc, Level: global, Init: True
+;Name: main, Type: undef, Kind: proc, Level: global, Init: True
